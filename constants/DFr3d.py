34b94b38317c92332dfab2e1345f34e89a12a916
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-05 13:39:02 -0400

    Update changelog for v0.7.1

--- constants/DFr3d.py
+++ constants/DFr3d.py
@@ -32,6 +32,6 @@ class DFr3d:
     )
     SERVICE_USER: Final[str] = "fr3d"
     VENV_DIRECTORY: Final[str] = ".venv"
-    VERSION: Final[str] = "0.7.0"
+    VERSION: Final[str] = "0.7.1"
     WATCHDOG_LOG: Final[Path] = Path("/opt/fr3d/logs/llm-watchdog.log")
     FRED_SERVER_LOG: Final[Path] = Path("/opt/fr3d/logs/fr3d.log")
