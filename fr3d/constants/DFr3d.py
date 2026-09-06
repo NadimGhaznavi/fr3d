439e10f7a30086924fe1dd343d0bb1ea52ffc8b2
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-06 07:39:32 -0400

    Update changelog for v0.9.0

--- fr3d/constants/DFr3d.py
+++ fr3d/constants/DFr3d.py
@@ -6,7 +6,7 @@ from fr3d.constants.DFile import DFileDef as DEFFILE
 
 class DFr3d:
     # Project version
-    VERSION: Final[str] = "0.8.15"
+    VERSION: Final[str] = "0.9.0"
 
     # Fr3d ZMQ network info
     HOST: Final[str] = "0.0.0.0"
