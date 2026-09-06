ebe09c1e9d8a0c5cbf957a8577399d18879a11cd
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-06 13:06:49 -0400

    Update changelog for v0.11.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.11.2] - 2026-09-06 @ 13:06
+
 ## [0.11.1] - 2026-09-06 @ 12:55
 
 ### Added
--- fr3d/constants/DFr3d.py
+++ fr3d/constants/DFr3d.py
@@ -6,7 +6,7 @@ from fr3d.constants.DFile import DFileDef as DEFFILE
 
 class DFr3d:
     # Project version
-    VERSION: Final[str] = "0.11.1"
+    VERSION: Final[str] = "0.11.2"
 
     # Fr3d ZMQ network info
     HOST: Final[str] = "0.0.0.0"
